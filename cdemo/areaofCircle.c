0fe4a725d2b074a80af4fbdc095d176bfefa4b70
Zhaos3646 <50209490+Zhaos3646@users.noreply.github.com>
2019-05-25 22:59:08 -0700

    Create areaofCircle.c

---
+++ cdemo/areaofCircle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+float areaofCircle(float radius) {
+  float area = radius*radius*M_PI;
+  return area;
+}
+
+int main() {
+  for (float i = 3.5; i < 12.5; i++) {
+     float solution = areaofCircle(i);
+     printf("%f\n", solution);
+  }
+}
