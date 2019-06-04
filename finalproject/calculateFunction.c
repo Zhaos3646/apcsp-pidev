732753596c465ab405c39d1011d452d58b755b53
Steven Zhao <steven.zhao@san.rr.com>
2019-06-04 09:16:29 -0700

    Updating

--- finalproject/calculateFunction.c
+++ finalproject/calculateFunction.c
@@ -2,8 +2,27 @@
 #include <math.h>
 #include <string.h>
 
-float currencyExchange (char[255] inputCountry, char[255] outputCountry) {
-    
+float currencyExchange (char inputString[255]) {
+    int count = 0;
+    char currencyFrom[255];
+    char currencyTo[255];
+    char currencyArray[][255] = {"euro", "pound", "rupee", "dollar"};
+    for(int i = 0; i<4; i++) {
+	if (strstr(inputString, currencyArray[i])) {
+	    count=count+1;
+	    if (count = 1) {
+	        printf("hi\n");
+	    }
+	    else if (count = 2) {
+		printf("no\n");
+	    }
+	    else {
+		printf("Too many currencies");
+		break;
+	    }
+	}
+    }
 }
 int main() {
+    currencyExchange("50 euro to dollars pounds");
 }
