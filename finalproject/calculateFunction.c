e5654dbb4706d71eae38ecae6b3a2010780fc32e
Steven Zhao <steven.zhao@san.rr.com>
2019-06-05 10:35:25 -0700

    Still broken

--- finalproject/calculateFunction.c
+++ finalproject/calculateFunction.c
@@ -2,23 +2,31 @@
 #include <math.h>
 #include <string.h>
 
+//char[255] removeSpace (char inputString[255]) {
+//    for (int i = 0; i < strlen(inputString); i++) {
+//	if (inputString[i] = " ") {
+//    }
+//}
+
 float currencyExchange (char inputString[255]) {
     int count = 0;
-    char* pos1;
-    char* pos2;
+    int* pos1;
+    int* pos2;
+    int index;
     char currencyFrom[255];
     char currencyTo[255];
+    char currencyTemp[255];
     char currencyArray[][255] = {"euro", "pound", "rupee", "dollar"};
     for(int i = 0; i<4; i++) {
 	if (strstr(inputString, currencyArray[i])) {
 	    count++;
 	    if (count == 1) {
 	        pos1 = strstr(inputString, currencyArray[i]);
-		printf("%d\n", pos1);
+		strcpy(currencyFrom, currencyArray[i]);
 	    }
 	    else if (count == 2) {
 		pos2 = strstr(inputString, currencyArray[i]);
-		printf("%d\n", pos2);
+		strcpy(currencyTo, currencyArray[i]);
 	    }
 	    else {
 		printf("Too many currencies\n");
@@ -26,7 +34,28 @@ float currencyExchange (char inputString[255]) {
 	    }
 	}
     }
+    if (count < 2) {
+	printf("Input 2 distinct currencies\n");
+	return;
+    }
+    if (pos1 > pos2) {
+	printf("To %s From %s\n", currencyTo, currencyFrom);
+	strcpy(currencyTemp, currencyFrom);
+	strcpy(currencyFrom, currencyTo);
+	strcpy(currencyTo, currencyTemp);
+	printf("To %s From %s\n", currencyTo, currencyFrom);
+    }
+    for (int i = 0; i < strlen(inputString); i++) {
+	if (inputString[i] = currencyFrom[0]) {
+	    index = i;
+	    break;
+	}
+    }
+    char money[index];
+    memcpy (money, &inputString[0], index);
+    money[strlen(money)] = '\0';
+    printf("%s\n", money);
 }
 int main() {
-    currencyExchange("50 euro to dollars pounds");
+    currencyExchange("50 dollars to euros");
 }
